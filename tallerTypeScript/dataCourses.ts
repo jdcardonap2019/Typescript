e5f461598dc15560ed27bcf8bc651a9bad0e3459
Juan David Cardona <jcardonapaez@gmail.com>
2020-10-21 22:13:47 -0500

    Taller Typescript1

---
+++ tallerTypeScript/dataCourses.ts
@@ -0,0 +1,9 @@
+import { Course } from './course.js';
+
+export const dataCourses = [
+  new Course("Desarrollo de Sw", "Sergio Yodeb", 3),
+  new Course("DALGO", "Rodrigo Cardoso", 3),
+  new Course("Sistemas Transaccionales", "Germán Bravo", 3),
+  new Course("Cálculo Vectorial", "Sergio Adarve", 3),
+  new Course("T.I Organizaciones", "Óscar González", 3)
+]
---
+++ tallerTypeScript/scripts/dataCourses.js
@@ -0,0 +1,8 @@
+import { Course } from './course.js';
+export var dataCourses = [
+    new Course("Desarrollo de Sw", "Sergio Yodeb", 3),
+    new Course("DALGO", "Rodrigo Cardoso", 3),
+    new Course("Sistemas Transaccionales", "Germán Bravo", 3),
+    new Course("Cálculo Vectorial", "Sergio Adarve", 3),
+    new Course("T.I Organizaciones", "Óscar González", 3)
+];
